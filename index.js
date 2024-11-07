65246aa1724c2f8278d9ec1ad473734924944726
Edis-mar <junioredismar18@gmail.com>
2024-11-07 18:14:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var leonardo = window.document.getElementById("leo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("seta-esquerda")
+var setadireita = window.document.getElementById("seta-direita")
+    leonardo.style ="display:none"
+    bruna.style ="display:flex"
+
+}
+function rolarparaesquerda() {
+    bruna.style ="display:none"
+    leonardo.style ="display:flex"
+}
