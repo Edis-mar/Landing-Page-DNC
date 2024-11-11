fa127c9c4ed233b246b098799fce6a20098e3b68
Edis-mar <junioredismar18@gmail.com>
2024-11-11 20:08:47 +0000

    carrosel terminado

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var samantha = window.document.getElementById("samantha")
 var bruna = window.document.getElementById("bruna")
 var setaesquerda = window.document.getElementById("seta-esquerda")
 var setadireita = window.document.getElementById("seta-direita")
+
+function rolarparadireita(){
     leonardo.style ="display:none"
     bruna.style ="display:flex"
-
+    setadireita.style = "display:none"
+    setaesquerda.style="display:flex"
 }
-function rolarparaesquerda() {
+function rolarparaesquerda(){
     bruna.style ="display:none"
     leonardo.style ="display:flex"
+    setadireita.style="display:flex"
+    setaesquerda.style="display:none"
 }
